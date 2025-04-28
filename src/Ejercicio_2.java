9c57c72104b02069793330278d054188bc55b171
StoianAlejandro <s.alejandrostoian@gmail.com>
2025-04-28 11:06:15 +0200

    Ejercicio 2 hecho

--- src/Ejercicio_2.java
+++ src/Ejercicio_2.java
@@ -1,5 +1,26 @@
-public class Ejercicio_2 {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Ejercicio_2 {
+    static Scanner sc = new Scanner(System.in);
+
+    public static String[] registrarAlumnos() {
+        System.out.println("Cuántos alumnos hay?");
+        int numAlumnos = sc.nextInt();
+        String[] alumnos = new String[numAlumnos];
+        System.out.println("Introduzca los nombres");
+        for (int i = 0; i < numAlumnos; i++) {
+            alumnos[i] = sc.next();
+        }
+        return alumnos;
+    }
+
+    public static void mostrarAlumnos(String[] alumnos) {
+        for (int i = 0; i < alumnos.length; i++) {
+            System.out.println(alumnos[i].toUpperCase());
+        }
+    }
+
+    public static void main (String[]args){
+        mostrarAlumnos(registrarAlumnos());
     }
 }
